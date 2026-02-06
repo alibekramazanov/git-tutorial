0edf1cf0cb6aaff17a1ebb1afa73306821007ebc
alibekramazanov <alibek.r.88@gmail.com>
2026-02-07 03:27:32 +0500

    Add examples.py example

---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,43 @@
+#ex1
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+#ex2
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#ex3
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#ex4
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#ex5
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
