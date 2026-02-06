22a4980dd15a49e31e542c627d8e13847844c622
alibekramazanov <alibek.r.88@gmail.com>
2026-02-07 03:17:29 +0500

    Add variables.py example

---
+++ Practice1/python-basics/variables.py
@@ -0,0 +1,55 @@
+#Ex1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#Ex2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#Ex3
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#Ex4
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Ex5
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#Ex6
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#Ex7
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#Ex8
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
